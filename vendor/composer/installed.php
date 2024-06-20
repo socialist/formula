5415d5354739a9c962c940dc1630e3062228b3ae
TimoLehnertz <57093983+TimoLehnertz@users.noreply.github.com>
2024-06-20 22:06:09 +0200

    removed version from composer.json

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'timo-lehnertz/formula',
-        'pretty_version' => '1.2.0',
-        'version' => '1.2.0.0',
+        'pretty_version' => '2.0.1',
+        'version' => '2.0.1.0',
         'reference' => NULL,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -272,8 +272,8 @@
             'dev_requirement' => true,
         ),
         'timo-lehnertz/formula' => array(
-            'pretty_version' => '1.2.0',
-            'version' => '1.2.0.0',
+            'pretty_version' => '2.0.1',
+            'version' => '2.0.1.0',
             'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
